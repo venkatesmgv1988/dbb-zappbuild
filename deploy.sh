3d9324617a8226b7e4f16989547e193657609107
Tamil <tamilarasan.pushparaj@capgemini.com>
2022-06-08 02:32:31 -0500

    Deploy script modified

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,17 @@
-#!/bin/sh
-##############################################################################################
-##
-##  This sample shell script is provided as an easy way to invoke the sample deploy script
-##  from the command line.
-##
-##  usage: deploy.sh
-##
-##  **NOTE - Build the application first to populate the work directory with build reports.
-##           The deploy scripts use the information in build report to create a ship list
-##           and then import a version to an UrbanCode Deploy component.
-##           UrbanCode Deploy agent must be installed and configured to import versions.
-##
-###############################################################################################
+#! /bin/sh
+## GitLab/Azure/Jenkins DBB Demo- UCD Buztool v1.3 (njl)
+. $HOME/.profile
+ucd_version=$1
+ucd_Component_Name=$2
+MyWorkDir=$3
+artifacts=$(ls -d $MyWorkDir/dbb-logs/build*)
+echo "**************************************************************"
+echo "**     Started:  UCD Publish on HOST/USER: $(uname -Ia) $USER"
+echo "**                                   Version:" $ucd_version
+echo "**                                 Component:" $ucd_Component_Name
+echo "**                                   workDir:" $MyWorkDir
+echo "**                         DBB Artifact Path:" $artifacts
 
-# Set the DBB bin directory
-DBB_HOME=/usr/lpp/IBM/dbb
+buzTool=/u/ucd/v7.0.4/bin/buztool.sh
 
-# $DBB_HOME/bin/groovyz automatically sets the env variables and classpath required for DBB
-CMD="$DBB_HOME/bin/groovyz deploy.groovy $@"
-
-echo $CMD
-$CMD
+groovyz $3/deploy.groovy -b $buzTool -w $artifacts -c $ucd_Component_Name -v $ucd_version
