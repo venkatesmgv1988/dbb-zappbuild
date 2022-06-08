e88c836680563746c77014abc382b09cf6654b38
Tamil <tamilarasan.pushparaj@capgemini.com>
2022-06-08 08:30:49 -0500

    Deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #! /bin/sh
 ## GitLab/Azure/Jenkins DBB Demo- UCD Buztool v1.3 (njl)
 . $HOME/.profile
+
 ucd_version=$1
+
 ucd_Component_Name=$2
+
 MyWorkDir=$3
+
 artifacts=$(ls -d $MyWorkDir/dbb-logs/build*)
-echo "**************************************************************"
-echo "**     Started:  UCD Publish on HOST/USER: $(uname -Ia) $USER"
-echo "**                                   Version:" $ucd_version
-echo "**                                 Component:" $ucd_Component_Name
-echo "**                                   workDir:" $MyWorkDir
-echo "**                         DBB Artifact Path:" $artifacts
 
 buzTool=/u/ucd/v7.0.4/bin/buztool.sh
 
