197152be18c7474cadd4b9083db3dc3207322a24
Administrator <Administrator@c2.4b.26a9.ip4.static.sl-reverse.com>
2022-06-29 11:26:47 -0500

    testing1

--- MortgageApplication/cobol/epscmort.cbl
+++ MortgageApplication/cobol/epscmort.cbl
@@ -75,7 +75,7 @@
        EPSCMORT-MAINLINE.
       * Call procedure to do SQL call
       *     PERFORM A805-DUMMY-SQL-CALL
-           DISPLAY 'UCD Testing'.
+           DISPLAY 'UCD Testing1'.
            MOVE LENGTH OF DFHCOMMAREA to W-COMAREA-LENGTH.
            MOVE DFHCOMMAREA to W-COMMUNICATION-AREA.
            EVALUATE TRUE
